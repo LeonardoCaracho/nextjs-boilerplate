a17cb4258bc15fb0e15e207591926e7d1a32aa22
negreyscow <leeo.apc@gmail.com>
2020-07-13 06:55:27 -0300

    set eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home({ title = "React Avançado" }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>{ title }</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Hello there!
+        </h1>
+      </main>
+    </div>
+  )
+}
