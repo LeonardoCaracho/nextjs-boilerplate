23dfa0e10d7cc54559fc0fb5c5a08015141c6050
negreyscow <leeo.apc@gmail.com>
2020-07-14 06:40:05 -0300

    prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import Head from 'next/head'
 
-export default function Home({ title = "React Avançado" }) {
+export default function Home({ title = 'React Avançado' }) {
   return (
     <div className="container">
       <Head>
-        <title>{ title }</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Hello there!
-        </h1>
+        <h1 className="title">Hello there!</h1>
       </main>
     </div>
   )
